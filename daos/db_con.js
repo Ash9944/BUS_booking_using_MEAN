38724cb5122671250f104b1fc5a8f48d94f1613f
Aswajith Prasad <ash99449@gmail.com>
2022-09-14 18:14:08 +0530

    removed schema and added new methods

---
+++ daos/db_con.js
@@ -0,0 +1,26 @@
+const mongodb = require('mongodb');
+
+const mongoclient = mongodb.MongoClient;
+
+
+const url  = 'mongodb://localhost:27017/BUS_APP'
+var dbCache = {}
+function connection(){
+    console.log("Database connected !")
+    const mong = mongoclient.connect(url)
+    dbCache.db = mong
+}
+
+connection()
+
+
+module.exports.getDb = function() {
+    return dbCache.db;
+}
+
+module.exports.getMongodb = function() {
+    return mongodb;    
+}
+
+
+module.exports.ObjectID = mongodb.ObjectID;
