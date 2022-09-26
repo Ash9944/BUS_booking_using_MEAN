f4abc33aebedec2b253f50d896cd19bf78b12e30
vijayrathnam <110458283+vijayrathnam@users.noreply.github.com>
2022-09-26 16:52:04 +0530

    minor changes

--- routes/bus.js
+++ routes/bus.js
@@ -4,43 +4,42 @@ const router = express.Router();
 const busController = require("../services/bus");
 
 router.post("/allbus", function (req, res) {
-    busController.getAllUserDetails().then((resp) => {
-        res.json({ data: resp })
-    })
+    busController.getAllUserDetails()
+        .then((resp) => res.json({ data: resp }))
+        .catch((err) => res.status(500).send({ error: err.name, message: err.message }))
 });
 
 router.get("/bus/:departure/:arrival", function (req, res) {
     query1 = { arr: req.params.arrival, dep: req.params.departure }
     query1.time = req.headers.data
-    busController.getBusinroutes(query1).then((resp) => {
-        res.json(resp)
-    })
+    busController.getBusinroutes(query1)
+        .then((resp) => res.json(resp))
+        .catch((err) => res.status(500).send({ error: err.name, message: err.message }))
 })
 
 router.post("/bus", function (req, res) {
-    busController.getBusinroutes(req.body).then((resp) => {
-        res.json(resp)
-    }
-    )
+    busController.getBusinroutes(req.body)
+        .then((resp) => res.json(resp))
+        .catch((err) => res.status(500).send({ error: err.name, message: err.message }))
 })
 
 router.post("/updbus", function (req, res) {
-    busController.updBus(req.body.query, req.body.detailsToUpdate).then((resp) => {
-        res.send("sucess !")
-    })
+    busController.updBus(req.body.query, req.body.detailsToUpdate)
+        .then((resp) => res.send("success !"))
+        .catch((err) => res.status(500).send({ error: err.name, message: err.message }))
 })
 
 router.post("/adddbus", function (req, res) {
-    busController.addBus(req.body).then((resp) => {
-        res.send("sucess !")
-    })
+    busController.addBus(req.body)
+        .then((resp) => res.send("success !"))
+        .catch((err) => res.status(500).send({ error: err.name, message: err.message }))
 })
 
 router.delete("/delbus", function (req, res) {
     console.log(req.body)
-    busController.deleteBus(req.body.employeeId).then((resp) => {
-        res.send("sucess !")
-    })
+    busController.deleteBus(req.body.employeeId)
+        .then((resp) => res.send("success !"))
+        .catch((err) => res.status(500).send({ error: err.name, message: err.message }))
 })
 
 module.exports = router;
--- routes/customer.js
+++ routes/customer.js
@@ -3,47 +3,47 @@ const router = express.Router();
 const customerController = require("../services/customer");
 
 router.get("/customers", function (req, res) {
-    customerController.getAllUserDetails().then((resp) => {
-        res.json({ data: resp })
-    })
+    customerController.getAllUserDetails()
+        .then((resp) => res.json({ data: resp }))
+        .catch((err) => res.status(500).send({ error: err.name, message: err.message }))
 })
 
 router.get("/customers/:id", (req, res) => {
-    customerController.getuser(req.params.id).then((resp) => {
-        res.json({ data: resp })
-    })
+    customerController.getuser(req.params.id)
+        .then((resp) => res.json({ data: resp }))
+        .catch((err) => res.status(500).send({ error: err.name, message: err.message }))
 })
 
 router.post("/addcustomers", (req, res) => {
-    customerController.addBooking(req.body).then((resp) => {
-        res.send(resp)
-    })
+    customerController.addBooking(req.body)
+        .then((resp) => res.send(resp))
+        .catch((err) => res.status(500).send({ error: err.name, message: err.message }))
 })
 
 router.post("/insertcustomers", (req, res) => {
     console.log(req.body)
-    customerController.addCustomer(req.body).then((resp) => {
-        res.send("success")
-    })
+    customerController.addCustomer(req.body)
+        .then((resp) => res.send("success"))
+        .catch((err) => res.status(500).send({ error: err.name, message: err.message }))
 })
 
 router.delete("/delcust", (req, res) => {
     console.log(req.body)
-    customerController.deletecustomer(req.body.employeeId).then((resp) => {
-        res.send("success !")
-    })
+    customerController.deletecustomer(req.body.employeeId)
+        .then((resp) => res.send("success !"))
+        .catch((err) => res.status(500).send({ error: err.name, message: err.message }))
 })
 
 router.post("/updcust", (req, res) => {
-    customerController.updateCustomer(req.body).then((resp) => {
-        res.send("success !")
-    })
+    customerController.updateCustomer(req.body)
+        .then((resp) => res.send("success !"))
+        .catch((err) => res.status(500).send({ error: err.name, message: err.message }))
 })
 
 router.post("/findcust", (req, res) => {
-    customerController.getcustbyquery(req.body).then((resp) => {
-        res.send(resp)
-    })
+    customerController.getcustbyquery(req.body)
+        .then((resp) => res.send(resp))
+        .catch((err) => res.status(500).send({ error: err.name, message: err.message }))
 })
 
 module.exports = router;
