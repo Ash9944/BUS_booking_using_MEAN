dcedc289dd40436f536e79bb2a9b038b9f444c88
vijayrathnam <110458283+vijayrathnam@users.noreply.github.com>
2022-09-29 12:25:45 +0530

    minor changes

---
+++ public/app/scripts/config/bus-booking.config.js
@@ -0,0 +1,42 @@
+(function () {
+    'use strict'
+    var myApp = angular.module("bus-booking", ['ui.router'])
+    myApp.config(configuration)
+    configuration.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider']
+    function configuration($stateProvider, $urlRouterProvider, $locationProvider) {
+        $locationProvider.html5Mode(false)
+        $urlRouterProvider.otherwise('/customer')
+        $stateProvider
+            .state('customers', {
+                url: "/customer",
+                templateUrl: 'app/modules/user.html',
+                controller: 'userCtrl',
+            })
+            .state('buses', {
+                url: "/bus/:id",
+                templateUrl: 'app/modules/bussearch.html',
+                controller: 'busctrl',
+            })
+            .state('userbus', {
+                url: "/user/:custid/:departure/:arrival",
+                templateUrl: 'app/modules/buses.html',
+                controller: 'userctrl',
+            })
+            .state('adminhome', {
+                url: "/adminhome",
+                templateUrl: 'app/modules/adminhomepage.html',
+                //controller: 'custctrl',
+            })
+            .state('customer', {
+                url: "/admin",
+                templateUrl: 'app/modules/busdbadmin.html',
+                controller: 'custctrl',
+            })
+            .state('customeradmin', {
+                url: "/custadmin",
+                templateUrl: 'app/modules/custdbadmin.html',
+                controller: 'adminctrl',
+            })
+
+    }
+})();
